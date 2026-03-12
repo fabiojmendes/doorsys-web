c4a82a18ea194ce0e6f9a6a768ff660720de9efe
Fabio Mendes <fabiojmendes@gmail.com>
2026-03-11 22:30:03 -0400

    Implement authentication using auth0

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { authGuard } from '@auth0/auth0-vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,5 +26,7 @@ const router = createRouter({
     }
   ]
 })
+
+router.beforeEach(authGuard)
 
 export default router
